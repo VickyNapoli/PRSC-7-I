3a0d0c3f5eae1d8acf78c59337964dd27e4f1b8f
VickyNapoli <107042306+VickyNapoli@users.noreply.github.com>
2023-06-16 20:25:52 -0300

    Update BADA HospedaMe.sql

--- Codigo/BADA HospedaMe.sql
+++ Codigo/BADA HospedaMe.sql
@@ -1,21 +1,24 @@
+CREATE DATABASE HospedaMe;
+
 CREATE TABLE Login (nombre_usuario VARCHAR(80) PRIMARY KEY,  
-    email VARCHAR(30) PRIMARY KEY, 
+    email VARCHAR(80) PRIMARY KEY, 
     contraseña VARCHAR(70));
 
 CREATE TABLE Usuario(id_usuario INT(5) PRIMARY KEY, 
     nombre_usuario VARCHAR(80), 
-    email VARCHAR(30),
+    email VARCHAR(80),
     nombre VARCHAR(40), 
     apellido VARCHAR(40), 
     dni INT(8), 
     direccion VARCHAR(100), 
+    Lista_Favoritos VARCHAR(250),
     FOREIGN KEY (nombre_usuario) REFERENCES Login(nombre_usuario),
     FOREIGN KEY (email) REFERENCES Login(email),
+    FOREIGN KEY (lista_favoritos) REFERENCES Favoritos(lista_favoritos));
 
-CREATE TABLE Favoritos (Iddelfavorito INT(5) NOT NULL AUTO_INCREMENT, 
-    id_publicacion INT(5),
-    PRIMARY KEY (Iddelfavorito),
-    FOREIGN KEY (id_publicacion) REFERENCES Publicacion(id_publicacion));
+CREATE TABLE Favoritos (Iddelfavorito INT(5) PRIMARY KEY, 
+    id_publicacion INT(5), 
+    cantidad INT(2));
 
 CREATE TABLE Comentarios (id_comentario INT(5) PRIMARY KEY, 
     id_publicacion INT(5), 
@@ -23,7 +26,8 @@ CREATE TABLE Comentarios (id_comentario INT(5) PRIMARY KEY,
     texto VARCHAR(255),
     FOREIGN KEY (id_publicacion) REFERENCES Publicacion(id_publicacion),
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario));
-
+    cantidad_estrellas INT(5));
+        
 CREATE TABLE Publicacion (id_publicacion INT(5) PRIMARY KEY, 
     id_usuario INT(5), 
     id_inmueble INT(5), 
@@ -32,14 +36,12 @@ CREATE TABLE Publicacion (id_publicacion INT(5) PRIMARY KEY,
     descripcion VARCHAR(250), 
     imagenes IMAGE, 
     cantidad_estrellas INT(5), 
+    url_pago VARCHAR(255),
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
     FOREIGN KEY (id_inmueble) REFERENCES Inmueble(id_inmueble));
 
-CREATE TABLE Inmueble (id_inmueble INT(5) PRIMARY KEY, 
-    id_publicacion INT(5), 
-    cantidad_ambientes INT(1), 
+CREATE TABLE Inmueble (id_inmueble INT(5) PRIMARY KEY,
     cantidad_personas INT(1), 
     cantidad_baños INT(1), 
     cantidad_metros INT(4),
-    url_pago VARCHAR(255),
     FOREIGN KEY (id_publicacion) REFERENCES Publicacion(id_publicacion));
