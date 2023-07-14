49e4ffd2c206756a27799b5697fe73031d44de28
VickyNapoli <107042306+VickyNapoli@users.noreply.github.com>
2023-07-14 10:56:40 -0300

    Update BASE ACTUALIZADA.sql

--- Codigo/BASE ACTUALIZADA.sql
+++ Codigo/BASE ACTUALIZADA.sql
@@ -1,25 +1,21 @@
 CREATE DATABASE HospedaMe;
-
 USE HospedaMe;
+
 CREATE TABLE Login(email VARCHAR(30) PRIMARY KEY, 
     contraseña VARCHAR(70));
 
-USE HospedaMe;
 CREATE TABLE Usuario(id_usuario INT(5) PRIMARY KEY, 
-    nombre_usuario VARCHAR(80), 
+    nombreyapellido VARCHAR(80), 
     email VARCHAR(30),
     nombre VARCHAR(40), 
-    apellido VARCHAR(40), 
-    dni INT(8), 
+    telefono INT(12), 
     direccion VARCHAR(100),
     FOREIGN KEY (email) REFERENCES Login(email));
 
-USE HospedaMe;
 CREATE TABLE Favoritos (Iddelfavorito INT(5) NOT NULL AUTO_INCREMENT, 
     id_publicacion INT(5),
     PRIMARY KEY (Iddelfavorito));
 
-USE HospedaMe;
 CREATE TABLE Comentarios (id_comentario INT(5) PRIMARY KEY, 
     id_publicacion INT(5), 
     id_usuario INT(5), 
@@ -27,7 +23,6 @@ CREATE TABLE Comentarios (id_comentario INT(5) PRIMARY KEY,
     FOREIGN KEY (id_publicacion) REFERENCES Publicacion(id_publicacion),
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario));
 
-USE HospedaMe;
 CREATE TABLE Publicacion (id_publicacion INT(5) PRIMARY KEY, 
     id_usuario INT(5), 
     id_inmueble INT(5), 
@@ -39,7 +34,6 @@ CREATE TABLE Publicacion (id_publicacion INT(5) PRIMARY KEY,
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
     FOREIGN KEY (id_inmueble) REFERENCES Inmueble(id_inmueble));
 
-USE HospedaMe;
 CREATE TABLE Inmueble (id_inmueble INT(5) PRIMARY KEY, 
     id_publicacion INT(5), 
     cantidad_ambientes INT(1), 
@@ -49,7 +43,6 @@ CREATE TABLE Inmueble (id_inmueble INT(5) PRIMARY KEY,
     url_pago VARCHAR(255),
     FOREIGN KEY (id_publicacion) REFERENCES Publicacion(id_publicacion));
 
-USE HospedaMe;
 ALTER TABLE Favoritos
 ADD CONSTRAINT id_publicacion
 FOREIGN KEY id_publicacion REFERENCES Publicacion(id_publicacion);
