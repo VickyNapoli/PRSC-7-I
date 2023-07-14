997feb33b208b453cd892245550168883110c412
Oriana Contreras <107041871+oricontreras@users.noreply.github.com>
2023-07-14 13:51:23 -0300

    Update BASE ACTUALIZADA.sql

--- Codigo/BASE ACTUALIZADA.sql
+++ Codigo/BASE ACTUALIZADA.sql
@@ -4,44 +4,43 @@ USE HospedaMe;
 CREATE TABLE Login(email VARCHAR(30) PRIMARY KEY, 
     contraseña VARCHAR(70));
 
-CREATE TABLE Usuario(id_usuario INT(5) PRIMARY KEY, 
+CREATE TABLE Usuario(id_usuario INT PRIMARY KEY, 
     nombreyapellido VARCHAR(80), 
     email VARCHAR(30),
-    telefono INT(12), 
+    telefono INT, 
     direccion VARCHAR(100),
     FOREIGN KEY (email) REFERENCES Login(email));
 
-CREATE TABLE Favoritos (Iddelfavorito INT(5) NOT NULL AUTO_INCREMENT, 
-    id_publicacion INT(5),
+CREATE TABLE Favoritos (Iddelfavorito INT NOT NULL AUTO_INCREMENT, 
+    id_publicacion INT,
     PRIMARY KEY (Iddelfavorito));
 
-CREATE TABLE Comentarios (id_comentario INT(5) PRIMARY KEY, 
-    id_publicacion INT(5), 
-    id_usuario INT(5), 
+CREATE TABLE Comentarios (id_comentario INT PRIMARY KEY, 
+    id_publicacion INT, 
+    id_usuario INT, 
     texto VARCHAR(255),
-    FOREIGN KEY (id_publicacion) REFERENCES Publicacion(id_publicacion),
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario));
 
-CREATE TABLE Publicacion (id_publicacion INT(5) PRIMARY KEY, 
-    id_usuario INT(5), 
-    id_inmueble INT(5), 
+CREATE TABLE Publicacion (id_publicacion INT PRIMARY KEY, 
+    id_usuario INT, 
+    id_inmueble INT, 
     direccion VARCHAR(100), 
-    precio INT(10), 
+    precio INT, 
     descripcion VARCHAR(250), 
     imagenes VARCHAR(255), 
-    cantidad_estrellas INT(5), 
+    cantidad_estrellas INT, 
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
     FOREIGN KEY (id_inmueble) REFERENCES Inmueble(id_inmueble));
 
-CREATE TABLE Inmueble (id_inmueble INT(5) PRIMARY KEY, 
-    id_publicacion INT(5), 
-    cantidad_ambientes INT(1), 
-    cantidad_personas INT(1), 
-    cantidad_baños INT(1), 
-    cantidad_metros INT(4),
+CREATE TABLE Inmueble (id_inmueble INT PRIMARY KEY, 
+    id_publicacion INT, 
+    cantidad_ambientes INT, 
+    cantidad_personas INT, 
+    cantidad_baños INT, 
+    cantidad_metros INT,
     url_pago VARCHAR(255),
     FOREIGN KEY (id_publicacion) REFERENCES Publicacion(id_publicacion));
 
-ALTER TABLE Favoritos
+ALTER TABLE Comentarios
 ADD CONSTRAINT id_publicacion
 FOREIGN KEY id_publicacion REFERENCES Publicacion(id_publicacion);
